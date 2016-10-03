124fbb76ceeb1e99bec2d3180429ad64df17e527
April Uzzle <april.uzzle@rochester.edu>
2016-10-02 23:36:55 -0400

    HTML Content

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Alternative Music</title>
+	</head>
+
+	<body>
+		<header>
+			<h1>Alternative Rock</h1>
+		</header>
+	
+		<article>
+			<p>Alternative rock (also called alternative music, alt-rock or simply alternative) is a genre of rock music that emerged from the independent music underground of the 1980s and became widely popular in the 1990s and 2000s. In this instance, the word "alternative" refers to the genre's distinction from mainstream rock music. The term's original meaning was broader, referring to a generation of musicians unified by their collective debt to either the musical style or simply the independent, D.I.Y. ethos of punk rock, which in the late 1970s laid the groundwork for alternative music.[4] At times, "alternative" has been used as a catch-all description for music from underground rock artists that receives mainstream recognition, or for any music, whether rock or not, that is seen to be descended from punk rock (including some examples of punk itself, as well as new wave, and post-punk).</p>
+
+			<p>Alternative rock is a broad umbrella term consisting of music that differs greatly in terms of its sound, its social context, and its regional roots. By the end of the 1980s magazines and zines, college radio airplay, and word of mouth had increased the prominence and highlighted the diversity of alternative rock, helping to define a number of distinct styles (and music scenes) such as gothic rock, jangle pop, noise pop, indie rock, indie pop, grunge, industrial rock, alternative hip hop, and rap rock. Most of these subgenres had achieved minor mainstream notice and a few bands representing them, such as Hüsker Dü and R.E.M., had even signed to major labels. But most alternative bands' commercial success was limited in comparison to other genres of rock and pop music at the time, and most acts remained signed to independent labels and received relatively little attention from mainstream radio, television, or newspapers. With the breakthrough of Nirvana and the popularity of the grunge and Britpop movements in the 1990s, alternative rock entered the musical mainstream and many alternative bands became successful.</p>
+
+			<p>By the end of the decade, alternative rock's mainstream prominence declined due to a number of events that caused grunge and Britpop to fade and led to the hiatus of the Lollapalooza festival. Nevertheless, post-grunge remained commercially viable into the start of the 21st century, with the commercial success of Creed and Matchbox Twenty; Radiohead's critical acclaim, and the success of some post-Britpop groups like Coldplay. Emo attracted attention in the larger alternative rock world, and the term was applied to a variety of artists, including multi-platinum acts. During the late 1990s and early 2000s, several alternative rock bands emerged, including The White Stripes, The Strokes, Arcade Fire, Franz Ferdinand, and Interpol, that drew from post-punk and new wave. Post-punk revival artists such as Modest Mouse and The Killers had commercial success in the early and mid 2000s.</p>
+
+
+
+		</article>
+
+		<aside>
+			<h2>Characteristics</h2>
+			<p>The name "alternative rock" essentially serves as an umbrella term for underground music that has emerged in the wake of punk rock since the mid-1980s.[19] Throughout much of its history, alternative rock has been largely defined by its rejection of the commercialism of mainstream culture, although this could be contested ever since some of the major alternative artists have achieved mainstream success or co-opted with the major labels from the 1990s onwards (especially since the new millennium and beyond). Alternative bands during the 1980s generally played in small clubs, recorded for indie labels, and spread their popularity through word of mouth.[20] As such, there is no set musical style for alternative rock as a whole, although The New York Times in 1989 asserted that the genre is "guitar music first of all, with guitars that blast out power chords, pick out chiming riffs, buzz with fuzztone and squeal in feedback."[21] Sounds range from the gloomy soundscapes of gothic rock to the jangling guitars of indie pop to the dirty guitars of grunge to the 1960s/1970s revivalism of Britpop. More often than in other rock-styles since the mainstreaming of rock music during the 1970s, alternative rock lyrics tend to address topics of social concern, such as drug use, depression, suicide, and environmentalism.[20] This approach to lyrics developed as a reflection of the social and economic strains in the United States and United Kingdom of the 1980s and early 1990s.[22]</p>
+
+		</aside>
+
+		<footer>
+		<h2>Citations</h2>
+		<p>
+			Content taken from <a href="https://en.wikipedia.org/wiki/Alternative_rock">https://en.wikipedia.org/wiki/Alternative_rock</a>.
+		</p>
+
+
+
+		</footer>
+
+
+
+</body>
+
+</html>
