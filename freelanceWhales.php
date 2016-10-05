f5033fba852a698a398d3e088748399ada93b260
Alex <amai2@u.rochester.edu>
2016-10-05 11:54:38 -0400

    Update grid settings

--- freelanceWhales.php
+++ freelanceWhales.php
@@ -19,61 +19,64 @@
 		include "inc/nav.inc"
 	 	?>
 
-	<section>	
-		<article class="col-md-7">
+	<section>
+		<div class="row">	
+			<article class="col-md-7">
 
-			<p>Freelance Whales is an American indie rock band which formed in Queens, New York, United States, in 2008. The band consists of frontman Judah Dadone (lead vocals, banjo, acoustic and electric guitar, synthesizer, bass), and bandmates Doris Cellar (bass, harmonium, glockenspiel, synthesizer, vocals), Chuck Criss (banjo, bass, synthesizer, glockenspiel, harmonium, acoustic and electric guitar, vocals), Jacob Hyman (drums, percussion, vocals) and Kevin Read (acoustic and electric guitar, glockenspiel, mandolin, synthesizer, vocals).</p>
+				<p>Freelance Whales is an American indie rock band which formed in Queens, New York, United States, in 2008. The band consists of frontman Judah Dadone (lead vocals, banjo, acoustic and electric guitar, synthesizer, bass), and bandmates Doris Cellar (bass, harmonium, glockenspiel, synthesizer, vocals), Chuck Criss (banjo, bass, synthesizer, glockenspiel, harmonium, acoustic and electric guitar, vocals), Jacob Hyman (drums, percussion, vocals) and Kevin Read (acoustic and electric guitar, glockenspiel, mandolin, synthesizer, vocals).</p>
 
-			
-			<h2>History</h2>
+				
+				<h2>History</h2>
 
-			<figure>
-				<img src="images/image5.jpg" alt="Freelance Whales">
-				<figcaption>Freelance Whales</figcaption>
-			</figure>
-			<p>Freelance Whales was founded during 2008,[1] after connecting through Craigslist and friends of friends.[2] The band's debut album, Weathervanes, was largely composed by frontman Judah Dadone, the lyrics based on a combination of childhood memories and dream journaling.[3] Because they're from New York, the band's name derives from that freelance atmosphere that the city has. Judah Dadone, once said: 'Everybody in New York is a freelancer of something, and we used to be too when we played in the subways'. Multi-instrumentalist Chuck Criss is the brother of actor and singer Darren Criss.[4]</p>
+				<figure>
+					<img src="images/image5.jpg" class="img-responsive" alt="Freelance Whales">
+					<figcaption>Freelance Whales</figcaption>
+				</figure>
+				<p>Freelance Whales was founded during 2008,[1] after connecting through Craigslist and friends of friends.[2] The band's debut album, Weathervanes, was largely composed by frontman Judah Dadone, the lyrics based on a combination of childhood memories and dream journaling.[3] Because they're from New York, the band's name derives from that freelance atmosphere that the city has. Judah Dadone, once said: 'Everybody in New York is a freelancer of something, and we used to be too when we played in the subways'. Multi-instrumentalist Chuck Criss is the brother of actor and singer Darren Criss.[4]</p>
 
-			<p>The band began to gain traction in October 2009 as they honed their sound on subway platforms around New York City. Toward the latter part of 2009, they self-released Weathervanes,[5] and on April 27, 2010 the album was re-released under the band's record labels, Frenchkiss and Mom + Pop.</p>
+				<p>The band began to gain traction in October 2009 as they honed their sound on subway platforms around New York City. Toward the latter part of 2009, they self-released Weathervanes,[5] and on April 27, 2010 the album was re-released under the band's record labels, Frenchkiss and Mom + Pop.</p>
 
-			<p>In September 2010, Twitter selected Freelance Whales' music as the background for a video[6] introducing a major rework of the service's user experience. The same song, entitled "Generator ^ First Floor", was also used by the NBC show Chuck in its 4th season premiere episode ("Chuck vs. the Anniversary"), which premiered September 20, 2010, as well as by Chevrolet in advertisements for the 2011 Chevy Volt. On October 12, 2010, the song "Broken Horse" appeared in an episode of One Tree Hill ("Nobody Taught Us to Quit"). Their song "The Great Estates" was also featured in an episode of the USA show Covert Affairs Season 1. Their song "Generator ^ Second Floor" was used on season 5, episode 1 of the British show Skins while "Location" was featured in episode 4. Their song "Starring" was used in season 2 My Life as Liz on MTV. "Starring" was also featured in an episode of Grey's Anatomy in Season 7. A 2011 Starbucks commercial also used "Generator ^ First Floor" as background music, as did the David Cornfield Melanoma Fund for their "Dear 16-year-old Me" melanoma awareness campaign. Their song "Hannah" was also used in the Australian television show Offspring. Their song "Location" from the Weathervanes album was featured in LOL, starring Miley Cyrus and Demi Moore.</p>
+				<p>In September 2010, Twitter selected Freelance Whales' music as the background for a video[6] introducing a major rework of the service's user experience. The same song, entitled "Generator ^ First Floor", was also used by the NBC show Chuck in its 4th season premiere episode ("Chuck vs. the Anniversary"), which premiered September 20, 2010, as well as by Chevrolet in advertisements for the 2011 Chevy Volt. On October 12, 2010, the song "Broken Horse" appeared in an episode of One Tree Hill ("Nobody Taught Us to Quit"). Their song "The Great Estates" was also featured in an episode of the USA show Covert Affairs Season 1. Their song "Generator ^ Second Floor" was used on season 5, episode 1 of the British show Skins while "Location" was featured in episode 4. Their song "Starring" was used in season 2 My Life as Liz on MTV. "Starring" was also featured in an episode of Grey's Anatomy in Season 7. A 2011 Starbucks commercial also used "Generator ^ First Floor" as background music, as did the David Cornfield Melanoma Fund for their "Dear 16-year-old Me" melanoma awareness campaign. Their song "Hannah" was also used in the Australian television show Offspring. Their song "Location" from the Weathervanes album was featured in LOL, starring Miley Cyrus and Demi Moore.</p>
 
-			<p>The band released their sophomore album, Diluvia, on October 9, 2012. On April 17, 2013, the band announced on Facebook that Doris Cellar would be leaving the band. On August 15, 2014, a new song by the band, titled "Hyde", was posted to The Wild Honey Pie's YouTube channel.[7]</p>
+				<p>The band released their sophomore album, Diluvia, on October 9, 2012. On April 17, 2013, the band announced on Facebook that Doris Cellar would be leaving the band. On August 15, 2014, a new song by the band, titled "Hyde", was posted to The Wild Honey Pie's YouTube channel.[7]</p>
 
-			<h2>Performing</h2>
-			<p>The band's first performance was at Staten Island's abandoned Farm Colony in January 2009. Throughout 2009, the Whales continued to busk around New York City, playing subway platforms and more traditional stages. In November–December 2009, Freelance Whales took on their first U.S./Canada tour, opening for London-based indie pop band, Fanfarlo. They continued to tour nationally over the course of 2010, as the first band of three on tour with Brooklyn-based rock band, Bear in Heaven, and indie-rock band, Cymbals Eat Guitars, and as the opening band for Swedish indie rock band, Shout Out Louds. They also began to dabble into the world of international touring, playing shows in England and releasing their first international single in the U.K. in February 2010.[8] Freelance Whales have since toured the U.S. and Canada with Canadian indie rock band Tokyo Police Club,[9] and as well as English indie rock band Foals and alternative New Zealand band, The Naked and Famous.[10]</p>
+				<h2>Performing</h2>
+				<p>The band's first performance was at Staten Island's abandoned Farm Colony in January 2009. Throughout 2009, the Whales continued to busk around New York City, playing subway platforms and more traditional stages. In November–December 2009, Freelance Whales took on their first U.S./Canada tour, opening for London-based indie pop band, Fanfarlo. They continued to tour nationally over the course of 2010, as the first band of three on tour with Brooklyn-based rock band, Bear in Heaven, and indie-rock band, Cymbals Eat Guitars, and as the opening band for Swedish indie rock band, Shout Out Louds. They also began to dabble into the world of international touring, playing shows in England and releasing their first international single in the U.K. in February 2010.[8] Freelance Whales have since toured the U.S. and Canada with Canadian indie rock band Tokyo Police Club,[9] and as well as English indie rock band Foals and alternative New Zealand band, The Naked and Famous.[10]</p>
 
-			<p>The band has received critical acclaim from a number of respected sources. In advance of their performances at South by Southwest in Austin, Texas, Spin magazine called them one of their 50 "Must Hear Bands at SXSW,"[11] and a live performance for All Songs Considered led NPR to refer to them as "a band to watch this year."[2]</p>
-		</article>
+				<p>The band has received critical acclaim from a number of respected sources. In advance of their performances at South by Southwest in Austin, Texas, Spin magazine called them one of their 50 "Must Hear Bands at SXSW,"[11] and a live performance for All Songs Considered led NPR to refer to them as "a band to watch this year."[2]</p>
+			</article>
 
-		<aside class="col-md-5">
-			<h2>Discography</h2>
-				<p>According to Metacritic, Freelance Whales' debut LP Weathervanes was received with generally favorable reviews, ranging from a 91/100 from Entertainment Weekly to a 4.2/10 from Pitchfork Media.[12] According to EW, the 13-track album, released April 27, 2010, is "the best electronic indie-pop debut since Ben Gibbard last tuned his laptop."[13] Many liken Dadone's vocals to those of Ben Gibbard of the electronic indie pop band The Postal Service or of American singer-songwriter Sufjan Stevens.</p>
+			<aside class="col-md-5">
+				<h2>Discography</h2>
+					<p>According to Metacritic, Freelance Whales' debut LP Weathervanes was received with generally favorable reviews, ranging from a 91/100 from Entertainment Weekly to a 4.2/10 from Pitchfork Media.[12] According to EW, the 13-track album, released April 27, 2010, is "the best electronic indie-pop debut since Ben Gibbard last tuned his laptop."[13] Many liken Dadone's vocals to those of Ben Gibbard of the electronic indie pop band The Postal Service or of American singer-songwriter Sufjan Stevens.</p>
 
-			<h3>Albums</h3>
-			<ul>
-				<li>Weathervanes (April 13, 2010)</li>
-				<li>Diluvia (October 9, 2012)</li>
-			</ul>
+				<h3>Albums</h3>
+				<ul>
+					<li>Weathervanes (April 13, 2010)</li>
+					<li>Diluvia (October 9, 2012)</li>
+				</ul>
 
-			<h3>EPs</h3>
-			<ul>
-				<li>The Benefit for Japan EP (May, 2011)</li>
-			</ul>
+				<h3>EPs</h3>
+				<ul>
+					<li>The Benefit for Japan EP (May, 2011)</li>
+				</ul>
 
-			<h3>Singles</h3>
-			<ul>
-				<li>"Hannah" (April 27, 2010)</li>
-				<li>"Enzymes" (October 2010)</li>
-				<li>"Day Off"</li>
-				<li>"Generator ^ First Floor" (March 2011)</li>
-				<li>"Spitting Image" (October 2012)[14]</li>
+				<h3>Singles</h3>
+				<ul>
+					<li>"Hannah" (April 27, 2010)</li>
+					<li>"Enzymes" (October 2010)</li>
+					<li>"Day Off"</li>
+					<li>"Generator ^ First Floor" (March 2011)</li>
+					<li>"Spitting Image" (October 2012)[14]</li>
 
-			</ul>
+				</ul>
 
-		</aside>
+			</aside>
+
+		</div>
 	</section>
 
-		<footer class="col-md-12">
+		<footer>
 			<p>Content taken from <a href="https://en.wikipedia.org/wiki/Freelance_Whales">https://en.wikipedia.org/wiki/Freelance_Whales</a>.</p>
 
 		</footer>
