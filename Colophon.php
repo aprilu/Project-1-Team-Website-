c9197fd18220f9417aed6243fbbeef6b93ccf772
Alex <amai2@u.rochester.edu>
2016-10-09 21:37:53 -0400

    Update colophon.php
    Added content in it

--- Colophon.php
+++ Colophon.php
@@ -1,22 +1,139 @@
 <!doctype html>
 <html>
 	<?php 
+	$thisPage = "CP"
+	 ?>
+	<?php 
 	include "inc/head.inc"
 	?>
 
 	<body>
 		<div class="container">
-			<section id="top-part">
-			<?php 
-			include "inc/nav.inc"
-		 	?>
-		 	
 			<header>
 				<h1>Colophon</h1>
 			</header>
-		 </section>
+			<?php 
+			include "inc/nav.inc"
+	 		?>
 
-		</div>
+	 		<div class="jumbotron">
+	 			<div class="container">
+	 				<h1>Who We Are</h1>
+	 				<p>We are a team of student web developers from the University of Rochester that have a passion for music. By leveraging our diverse background and interests, we created a website to showcase a few of our favorite alternative rock bands.</p>
+	 				<p><a class="btn btn-primary btn-lg" href="#aboutUs">Learn more &raquo;</a></p>
+	 			</div>
+	 		</div>
+
+	 		<!-- Team Information & Who Did What -->
+	 		<section>
+	 			<h2 id="aboutUs">About Us</h2>
+	 			<div class="row">
+	 				<div class="col-md-4">
+	 					<img src="images/sarah.jpg" class="img-responsive" alt="Sarah">
+	 				</div>
+
+	 				<div class="col-md-8">
+	 					<h3>Sarah Kingsley</h3>
+	 					<ul>
+	 						<li>CSS Artist</li>
+	 						<li>###</li>
+	 						<li>###</li>
+	 					</ul>
+	 				</div>
+	 			</div>
+
+	 			<br><hr/>
+	 			<div class="row">
+	 				<div class="col-md-4">
+	 					<img src="images/alex.jpg" class="img-responsive" alt="Alex">
+	 				</div>
+
+	 				<div class="col-md-8">
+	 					<h3>Alex Mai</h3>
+	 					<ul>
+	 						<li>Framework Wrangler</li>
+	 						<li>###</li>
+	 						<li>###</li>
+	 					</ul>
+	 				</div>
+	 			</div>
+
+	 			<br><hr/>
+	 			<div class="row">
+	 				<div class="col-md-4">
+	 					<img src="images/tess.jpg" class="img-responsive" alt="Tess">
+	 				</div>
+
+	 				<div class="col-md-8">
+	 					<h3>Tess Stone</h3>
+	 					<ul>
+	 						<li>Content Master and Designer</li>
+	 						<li>###</li>
+	 						<li>###</li>
+	 					</ul>
+	 				</div>
+	 			</div>
+
+	 			<br><hr/>
+	 			<div class="row">
+	 				<div class="col-md-4">
+	 					<img src="images/april.jpg" class="img-responsive" alt="April">
+	 				</div>
+
+	 				<div class="col-md-8">
+	 					<h3>April Uzzle</h3>
+	 					<ul>
+	 						<li>Architect</li>
+	 						<li>###</li>
+	 						<li>###</li>
+	 					</ul>
+	 				</div>
+	 			</div>
+
+	 			<br><hr/>
+	 			<div class="row">
+	 				<div class="col-md-4">
+	 					<img src="images/theresa.jpg" class="img-responsive" alt="Theresa">
+	 				</div>
+
+	 				<div class="col-md-8">
+	 					<h3>Theresa Xu</h3>
+	 					<ul>
+	 						<li>Javascript Coder</li>
+	 						<li>###</li>
+	 						<li>###</li>
+	 					</ul>
+	 				</div>
+	 			</div>
+	 			<br><hr><br>
+	 		</section>
+
+	 		<!-- Technologies Used -->
+	 		<section>
+	 			<h2>Technologies Used For All Pages</h2>
+	 			<ul>	
+	 				<li>PHP</li>
+	 				<li>HTML</li>
+	 				<li>CSS</li>
+	 				<li>Javascript</li>
+	 				<li>Bootstrap</li>
+	 			</ul>
+	 			<br><hr><br>
+	 		</section>
+
+	 		<!-- Sources of Content -->
+	 		<section>
+	 			<h2>Sources</h2>
+	 			<ol>
+	 				<li><a href="https://en.wikipedia.org/wiki/Alternative_rock">https://en.wikipedia.org/wiki/Alternative_rock</a></li>
+	 				<li><a href="https://en.wikipedia.org/wiki/Neutral_Milk_Hotel">https://en.wikipedia.org/wiki/Neutral_Milk_Hotel</a></li>
+	 				<li><a href="https://en.wikipedia.org/wiki/Lord_Huron">https://en.wikipedia.org/wiki/Lord_Huron</a></li>
+	 				<li><a href="http://bits.blogs.nytimes.com/2014/02/19/meet-lord-huron-a-musical-project-that-is-also-an-alternate-reality-game/?_r=0">http://bits.blogs.nytimes.com/2014/02/19/meet-lord-huron-a-musical-project-that-is-also-an-alternate-reality-game/?_r=0</a></li>
+	 				<li><a href="https://en.wikipedia.org/wiki/Freelance_Whales">https://en.wikipedia.org/wiki/Freelance_Whales</a></li>
+	 			</ol>
+	 		</section>
+
+		</div> <!-- end .container -->
 	</body>
 
 </html>
