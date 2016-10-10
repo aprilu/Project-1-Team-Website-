8b06eb64abab77c65d501df05202c3b81175694a
Alex <amai2@u.rochester.edu>
2016-10-09 20:55:52 -0400

    Fixed Bootstrap and Add Colophon
    Made it more dynamic to accommodate more HTML added

---
+++ Colophon.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+	<?php 
+	include "inc/head.inc"
+	?>
+
+	<body>
+		<div class="container">
+			<section id="top-part">
+			<?php 
+			include "inc/nav.inc"
+		 	?>
+		 	
+			<header>
+				<h1>Colophon</h1>
+			</header>
+		 </section>
+
+		</div>
+	</body>
+
+</html>
