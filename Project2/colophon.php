c8a6bd774e0b06acc97cb6371d4b3668bc7becd3
April Uzzle <april.uzzle@rochester.edu>
2016-11-20 14:00:24 -0500

    colophon initial fix

--- Project2/colophon.php
+++ Project2/colophon.php
@@ -46,22 +46,30 @@
             <div class="col-lg-12 colophon">
                 <div class="well text-center">
                     <h1>Who did what?</h1>
+                    <br>
                     <div class="members">
-                        <p>Gunnar Zemering - The Architect</p>
-                        <p>Created the project description and master plan. Chose the content and the framework. Installed the framework, set up the pages, chose the general order of content. Oversaw major design choices. Completed the PHP navbar and footer. Handled all GitHub conflicts and branch merging. Created the content outline for the presentation.<p>
-
-                        <p>Daniel Diaz-Etchevehere - The Javascript Coder </p>
-                        <p>Added the Bootstrap Carousel and its images to the index page (<a href="http://www.w3schools.com/bootstrap/bootstrap_carousel.asp">source</a>). I made a separate include and css files for the carousel for the sake of modularity. Found the unveil.js (<a href="http://luis-almeida.github.io/unveil/">source</a>) lazy loading plugin to increase performance and load images with animation. This involved adding a "data-src" attribute to every image to be loaded with this plugin, replacing the src with a placeholder image, and adding a script at the end of each php page to execute unveil.js on a specific class I called "unveilImg". To animate the image loading I added a couple lines of CSS: one to make the initial opacity of each image 0 and second using the field "transition" to have the image fade in.  
-
-                        <p>Ruairi Conway - The CSS Artist</p>
-                        <p>Designed a custom CSS to give style and structure to individual elements of the page. Based on the theme of our content, the color scheme and typeface of the site was inspired by SoundCloud.</p>
-
-                        <p>Teron Russell - The Wrangler of Frameworks</p>
+                    <div class="col-md-6" class="col-xs-6">
+                       <h3>Gunnar Zemering - The Architect</h3>
+                        <p>Created the project description and master plan. Chose the content and the framework. Installed the framework, set up the pages, chose the general order of content. Oversaw major design choices. Completed the PHP navbar and footer. Handled all GitHub conflicts and branch merging. Created the content outline for the presentation.<p> 
+                    </div>
+                    <div class="col-md-6" class="col-xs-6">
+                       <h3>Daniel Diaz-Etchevehere - The Javascript Coder </h3>
+                        <p>Added the Bootstrap Carousel and its images to the index page (<a href="http://www.w3schools.com/bootstrap/bootstrap_carousel.asp">source</a>). I made a separate include and css files for the carousel for the sake of modularity. Found the unveil.js (<a href="http://luis-almeida.github.io/unveil/">source</a>) lazy loading plugin to increase performance and load images with animation. This involved adding a "data-src" attribute to every image to be loaded with this plugin, replacing the src with a placeholder image, and adding a script at the end of each php page to execute unveil.js on a specific class I called "unveilImg". To animate the image loading I added a couple lines of CSS: one to make the initial opacity of each image 0 and second using the field "transition" to have the image fade in.   
+                    </div>
+                    <div class="col-md-6" class="col-xs-6">
+                        <h3>Ruairi Conway - The CSS Artist</h3>
+                        <p>Designed a custom CSS to give style and structure to individual elements of the page. Based on the theme of our content, the color scheme and typeface of the site was inspired by SoundCloud.</p> 
+                    </div>
+                    <div class="col-md-6" class="col-xs-6">
+                        <h3>Teron Russell - The Wrangler of Frameworks</h3>
                         <p>Removed Call to Action Bar that was present on the main page and the sub pages.Removed Call to Action Button from the Main Page. Added buttons under the genre introduction column which linked to its respective page.
-                        </p>
-
-                        <p>Jake Socolow - The Content Master and Designer</p>
+                        </p> 
+                    </div>
+                    <div class="col-md-6" class="col-xs-6">
+                        <h3>Jake Socolow - The Content Master and Designer</h3>
                         <p>Added text and images to all pages. Filled pages with text relevant to the subject matter. Added images for each individual artist, and for the genres themselves. Adjusted the size of the images to fit specific sizes. (Text from Wikipedia)</p>
+                    </div>
+            
                     </div>
                 </div>
             </div>
